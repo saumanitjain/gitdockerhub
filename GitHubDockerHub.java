f629249bebdb6a0a777e294f5f3e91c3533d0cc6
sjajain <saurabh.k.jain@oracle.com>
2016-09-29 20:34:01 +0530

    version 1

--- GitHubDockerHub.java
+++ GitHubDockerHub.java
@@ -3,8 +3,8 @@ public class GitHubDockerHub {
 
 	public static void main(String[] args){
 		
-		System.out.println("Git Hub");
-		System.out.println("Docker Hub");
+		System.out.println("Git Hub v1");
+		System.out.println("Docker Hub v1");
 	}
 	
 }
